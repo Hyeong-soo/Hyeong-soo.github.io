a9f5544e24cc24da03d24714d2cd83aaaf34a094
hyeongsoo <hyeongsoo@postech.ac.kr>
2023-01-15 03:05:18 +0900

    last commit

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d4fc025a5862311f866f9ba8b9fae2c1",
-"index.html": "ef1150ccf2123ce9805396ea3117a020",
-"/": "ef1150ccf2123ce9805396ea3117a020",
+"index.html": "047b8129d1d1d9c4d0de5159a66a02dc",
+"/": "047b8129d1d1d9c4d0de5159a66a02dc",
 "main.dart.js": "81b77aa65478aadc77fafb9a7e759d00",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
